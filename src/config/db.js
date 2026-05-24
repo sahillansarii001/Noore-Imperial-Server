03a8dac9966b01c8f30f15143a497fb98ab1dccb
Sahil Ansari <sahillansarii@gmail.com>
2026-05-24 20:44:33 +0530

    ...

--- src/config/db.js
+++ src/config/db.js
@@ -5,6 +5,7 @@ const { Pool } = pg;
 
 export const pool = new Pool({
   connectionString: env.DATABASE_URL,
+  ssl: env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 pool.on('error', (err, client) => {
